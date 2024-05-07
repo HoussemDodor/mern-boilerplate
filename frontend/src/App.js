4779e68cf1b2629b85cf9c28ea8196d167e9b12a
Houssem Dodor <houssem@dodor.nl>
2024-05-07 17:09:04 +0200

    installed tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
-
-
 function App() {
   return (
-    <div className="App">
+    <div className="bg-neutral-900 h-screen">
+      <span className="text-white inline-block align-middle">Hello World!</span>
     </div>
   );
 }

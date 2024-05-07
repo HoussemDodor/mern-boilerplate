6cedb1db9f47e0f3fa641b7b6c02b75042d002f9
Houssem Dodor <houssem@dodor.nl>
2024-05-07 11:11:38 +0200

    initialized backend

---
+++ backend/server.js
@@ -0,0 +1,52 @@
+//
+//  Imports
+//
+
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+
+// Route imports
+
+
+// Middleware imports
+const verifyJWT = require("./middleware/verifyJWT");
+const credentials = require("./middleware/credentials")
+
+//
+//  Uses
+//
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(credentials);
+app.use(cors(corsOptions));
+app.use(errorHandler)
+app.use((req, res, next) => {
+  console.log(req.path, req.method, req.body);
+  next();
+});
+
+// Unprotected Routes
+app.use("/api/user", userRoutes)
+
+// Protected Routes
+app.use(verifyJWT)
+app.use("/api/workouts", workoutRoutes);
+
+// DB connection and start app
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("DB Connection succesfull and the app is running");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });

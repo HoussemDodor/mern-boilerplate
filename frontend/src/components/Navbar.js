6a37b493da4b9b378c2cb91e9f34603df87e33ff
Houssem Dodor <houssem@dodor.nl>
2024-05-14 14:14:40 +0200

    Added HeroIcons

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
 import { useAuthContext } from "../hooks/useAuthContext";
+import { ArrowLeftStartOnRectangleIcon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
   const { logout } = useLogout();
@@ -12,17 +13,24 @@ const Navbar = () => {
         <h1 className="text-m md:text-2xl font-bold">MERN Boilerplate</h1>
       </Link>
       <nav>
-        <div className="">
-          {user ? (<>
-          <span className="py-3 px-5">{user.email}</span>
-          <button className="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300" onClick={logout}>Log out</button>
-          </>
+        <div className="flex items-center justify-center">
+          {user ? (
+            <>
+              <span className="py-3 px-5">{user.email}</span>
+              <button className="" onClick={logout}>
+                <ArrowLeftStartOnRectangleIcon className="size-6 text-gray-800 hover:scale-110 duration-300" />
+              </button>
+            </>
           ) : (
             <>
-              <Link to="/login" className="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300">Login</Link>
+              <Link
+                to="/login"
+                className="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300"
+              >
+                Log in
+              </Link>
             </>
           )}
-          
         </div>
       </nav>
     </header>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,9 @@
+import { HomeIcon } from "@heroicons/react/24/solid";
+
 const Home = () => {
   return (
     <div className="bg-gray-50 h-screen flex justify-center items-center">
+      <HomeIcon className="size-12 text-black" />
       <span className="text-black text-5xl">Hello World!</span>
     </div>
   );
